2c431dcbe1257a1379196299800550f0c9598a9b
Joannis Orlandos <joannis@orlandos.nl>
2020-01-03 14:31:08 +0100

    Remove IkigaJSON for encoding until the encoder is fixed for the complex use cases

--- Sources/AMKit/SecRouter/SecRouter.swift
+++ Sources/AMKit/SecRouter/SecRouter.swift
@@ -62,11 +62,8 @@ extension SecuredContent {
             }
                 
             return preEncode(self, for: request).flatMapThrowing {
-                var encoder = IkigaJSONEncoder()
-                // Nil as null is required for secured encodable to produce valid JSON
-                // This is due to an IkgiaJSON Bug
-                encoder.settings.encodeNilAsNull = true
-                encoder.settings.dateDecodingStrategy = .iso8601
+                var encoder = JSONEncoder()
+                encoder.dateDecodingStrategy = .iso8601
                 
                 SecurityHelper.updateContext(in: &encoder.userInfo, forSubject: self)
                 let context = try SecurityContext(from: request)
@@ -80,12 +77,9 @@ extension SecuredContent {
     }
     
     public func encodeResponseWithoutPreEncoding(for request: Request) -> EventLoopFuture<Response> {
-        var encoder = IkigaJSONEncoder()
+        var encoder = JSONEncoder()
         
-        // Nil as null is required for secured encodable to produce valid JSON
-        // This is due to an IkgiaJSON Bug
-        encoder.settings.encodeNilAsNull = true
-        encoder.settings.dateDecodingStrategy = .iso8601
+        encoder.dateDecodingStrategy = .iso8601
         
         do {
             SecurityHelper.updateContext(in: &encoder.userInfo, forSubject: self)
